84c96f22313600cc5d26905997b3c4805f1b078c
William Gu <74311720+stationarybird@users.noreply.github.com>
2024-09-12 02:53:46 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from datetime import date
 
 st.title("Finance Models")
     
-st.page_link("pages/blackscholes.py", label="Black Scholes Model", icon="📈")
-st.page_link("pages/binomial.py", label="Binomial Option Model", icon="📈")
+st.page_link("/pages/blackscholes.py", label="Black Scholes Model", icon="📈")
+st.page_link("/pages/binomial.py", label="Binomial Option Model", icon="📈")
 
         
 
